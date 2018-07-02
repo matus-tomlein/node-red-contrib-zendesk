15b9fd94bfbbcf1cf61db329efcac4ef24ee7e49
Matúš Tomlein <matus.tomlein@gmail.com>
2018-07-02 15:24:56 +0200

    Initial commit

---
+++ zendesk.js
@@ -0,0 +1,65 @@
+module.exports = function (RED) {
+  const zendesk = require('node-zendesk');
+
+  function ZendeskConfig(config) {
+    RED.nodes.createNode(this, config);
+
+    var node = this;
+
+    node.connect = function () {
+      if (!node.client) {
+        node.client = zendesk.createClient({
+          username: node.credentials.username,
+          token: node.credentials.token,
+          remoteUri: config.remoteUri
+        });
+      }
+    };
+
+  }
+
+  RED.nodes.registerType('ZendeskConfig', ZendeskConfig, {
+    credentials: {
+      username: {type: 'text'},
+      token: {type: 'password'}
+    }
+  });
+
+  function Zendesk(config) {
+    RED.nodes.createNode(this, config);
+
+    var node = this;
+
+    node.zendeskConfig = RED.nodes.getNode(config.zendeskConfig);
+
+    if (node.zendeskConfig) {
+      node.zendeskConfig.connect();
+
+      node.on('input', function (msg) {
+        var cb = function(err, req, result) {
+          if (err || req.statusCode >= 400) {
+            node.error(err, msg);
+          } else {
+            msg.payload = result;
+            node.send(msg);
+          }
+        };
+
+        var ticket = {
+          ticket: JSON.parse(JSON.stringify(msg.payload))
+        };
+
+        if (msg.ticketId) {
+          node.zendeskConfig.client.tickets.update(msg.ticketId, ticket, cb);
+        } else {
+          node.zendeskConfig.client.tickets.create(ticket, cb);
+        }
+      });
+    }
+    else {
+      node.error('ZenDesk connection not configured');
+    }
+  }
+
+  RED.nodes.registerType('zendesk', Zendesk);
+};
